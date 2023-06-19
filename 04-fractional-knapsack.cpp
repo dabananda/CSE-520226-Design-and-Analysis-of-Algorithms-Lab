d9b22164e6648ecbadf786f6c5547186389d3576
Dabananda Mitra <imdmitra@gmail.com>
2023-06-19 15:42:19 +0600

    Fractional Kanpsack

--- 04-fractional-knapsack.cpp
+++ 04-fractional-knapsack.cpp
@@ -1,44 +1,47 @@
-#include <algorithm>
+// Dabananda Mitra, CSE (2019-20), ISTT.
+// Email: imdmitra@outlook.com
+
 #include <iostream>
 
 using namespace std;
 
-void knapsack(int n, double w[], double p[], double m) {
-  double arr[n] = {0}, tp = 0;
+float knapsack(int n, float p[], float w[], int m) {
   int i;
+  float pw[n], tp = 0;
+  for (int i = 0; i < n; i++) {
+    pw[i] = p[i] / w[i];
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (pw[i] < pw[j]) {
+        swap(pw[i], pw[j]);
+        swap(p[i], p[j]);
+        swap(w[i], w[j]);
+      }
+    }
+  }
   for (i = 0; i < n; i++) {
     if (w[i] > m) {
       break;
     } else {
-      arr[i] = 1, tp += p[i], m -= w[i];
+      tp += p[i];
+      m -= w[i];
     }
   }
   if (i < n) {
-    arr[i] = m / w[i];
-    tp += (arr[i] * p[i]);
+    tp += (m / w[i]) * p[i];
   }
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << endl;
-  cout << tp << endl;
+  return tp;
 }
 
 int main() {
   int n, m;
   cin >> n >> m;
-  double w[n], p[n], pw[n];
-  for (int i = 0; i < n; i++) cin >> w[i] >> p[i];
-  for (int i = 0; i < n; i++) pw[i] = p[i] / w[i];
+  float p[n], w[n];
   for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (pw[i] < pw[j]) {
-        swap(pw[i], pw[j]);
-        swap(w[i], w[j]);
-        swap(p[i], p[j]);
-      }
-    }
+    cin >> p[i] >> w[i];
   }
-  knapsack(n, w, p, m);
+  cout << knapsack(n, p, w, m) << endl;
+
   return 0;
 }
